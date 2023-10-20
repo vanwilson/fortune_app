c5346add40ef9d24b2265b35dfb3b67a00972e00
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-10-20 14:30:07 -0500

    clean up fortune code to keep in raw data form

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,8 +1,7 @@
 class MyExamplesController < ApplicationController
   def fortune_method
     array = ["Beware a windfall of money.", "Follow your stomach.", "Pick the other one."]
-    number = rand(3)
-    render json: array[number]
+    render json: { fortune: array.sample } # need to keep it in raw data form, so include hashes and arrays so that users know how to access the data.
   end
 
   def lotto_method
@@ -20,4 +19,10 @@ class MyExamplesController < ApplicationController
   #   number = 0
   #   render json: number
   # end                    Can't do track page visits until we learn how to interact with the data in the database.
+
+  # def bottles_of_beer_method               ran out of time
+  #   count = 99
+  #   while count < 100 && > 0
+  #     render json: "#{number} bottles of beer on the wall"
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   get "/fortune", controller: "my_examples", action: "fortune_method"
   get "/lotto", controller: "my_examples", action: "lotto_method"
+  get "/bottles_of_beer", controller: "my_examples", action: "bottles_of_beer_method"
 end
